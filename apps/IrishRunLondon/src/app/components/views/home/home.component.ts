2eb9c8249b5ec11e68d2063a9e73dc997258c2a1
davidjdonnellan <davidd294@gmail.com>
2025-05-14 23:44:20 +0100

    feat: added home page

--- apps/IrishRunLondon/src/app/components/views/home/home.component.ts
+++ apps/IrishRunLondon/src/app/components/views/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+ scrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.scrolled = window.scrollY > 50;
+  }
 }
