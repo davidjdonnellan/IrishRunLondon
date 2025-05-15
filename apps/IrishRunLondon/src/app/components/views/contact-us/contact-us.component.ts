9a1e614a80694b3bc4490d5baa4a8f11fa9b927c
davidjdonnellan <davidd294@gmail.com>
2025-05-15 12:51:39 +0100

    feat: added organiser pics

--- apps/IrishRunLondon/src/app/components/views/contact-us/contact-us.component.ts
+++ apps/IrishRunLondon/src/app/components/views/contact-us/contact-us.component.ts
@@ -11,23 +11,23 @@ import { PanelComponent } from '../../common/panel/panel.component';
 export class ContactUsComponent {
   organisers = [
     {"name":"Pearse",
-    "img":'main.jpg',
+    "img":'pearse.jpg',
     "description":" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
     },
      {"name":"Emma",
-    "img":'emma.svg',
+    "img":'emma.jpg',
     "description":" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
     },
      {"name":"David",
-    "img":'david.svg',
+    "img":'david.jpg',
     "description":" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
     },
      {"name":"Adrian",
-    "img":'adrian.svg',
+    "img":'adrian.jpg',
     "description":" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
     },
      {"name":"Martina",
-    "img":'martina.svg',
+    "img":'martina.jpg',
     "description":" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
     },
   ]
