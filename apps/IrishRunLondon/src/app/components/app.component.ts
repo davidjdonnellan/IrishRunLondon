3ba3a7aa36bbae114de9d51afc55d01b1d9d6c43
davidjdonnellan <davidd294@gmail.com>
2025-05-15 11:08:38 +0100

    feat: add site footer and links to social media account

--- apps/IrishRunLondon/src/app/components/app.component.ts
+++ apps/IrishRunLondon/src/app/components/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import {FooterComponent} from './footer/footer.component'
 
 @Component({
-  imports: [NavigationComponent, RouterModule],
+  imports: [NavigationComponent, RouterModule, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
