dd52571dd666d03b471381055e0e003350d2d99a
davidjdonnellan <davidd294@gmail.com>
2025-05-15 19:09:03 +0100

    feat: added font-aswesome and event card component wip

--- apps/IrishRunLondon/src/app/components/views/events/events.component.ts
+++ apps/IrishRunLondon/src/app/components/views/events/events.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from '../../common/card/card.component';
 
 @Component({
   selector: 'app-events',
-  imports: [],
+  imports: [CommonModule,CardComponent],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
 export class EventsComponent {
+    events = [
+      { "event":"5K Run",
+        "location":"Hyde Park Corner",
+        "image":"",
+        "coordinates":"",
+        "date":"02/04/2025",
+        "time":"9:00am",
+        "bagDrop":"false",
+      },
+      { "event":"5K Run",
+        "location":"London Irish Centre Camden",
+        "image":"",
+        "coordinates":"",
+        "date":"15/05/2025",
+        "time":"18:30pm",
+        "bagDrop":"true",
+        "description":""
+      },
+      { "event":"Social",
+        "location":"London Irish Centre Camden",
+        "image":"",
+        "coordinates":"",
+        "date":"15/05/2025",
+        "time":"18:30pm",
+        "bagDrop":null,
+        "description":""
+      },
 
+    ]
 }
