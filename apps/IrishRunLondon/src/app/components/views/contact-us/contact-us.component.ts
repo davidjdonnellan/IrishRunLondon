3bd5f307cf2e88778f48327d69997929d9c144a6
davidjdonnellan <davidd294@gmail.com>
2025-05-15 11:46:24 +0100

    feat: added panel component and updates to contact us page

--- apps/IrishRunLondon/src/app/components/views/contact-us/contact-us.component.ts
+++ apps/IrishRunLondon/src/app/components/views/contact-us/contact-us.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PanelComponent } from '../../common/panel/panel.component';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [PanelComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
